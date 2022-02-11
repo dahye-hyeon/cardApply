f3f9c02ae39033786d937a828475939a5381d5b5
현다혜 <hdhzzv@gmail.com>
2022-02-11 16:59:59 +0900

    수정

--- CreditCard/src/main/java/com/sunrise/creditcard/dao/CardDAO.java
+++ CreditCard/src/main/java/com/sunrise/creditcard/dao/CardDAO.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.sunrise.creditcard.vo.CardVO;
 import com.sunrise.creditcard.vo.RcvApplVO;
 
 @Repository
